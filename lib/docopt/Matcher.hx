ce44b03c4c1e5f149659ed477df5ca8cf7781f95
Jonas Malaco Filho <jonas@jonasmalaco.com>
2015-11-02 19:07:02 -0200

    Make match (matchExpr) always safe

--- lib/docopt/Matcher.hx
+++ lib/docopt/Matcher.hx
@@ -33,21 +33,6 @@ class Matcher {
 		return res;
 	}
 
-	static function tryMatchExpr(args:List<ArgumentToken>, expr:Expr, opts:Map<String, Option>, res:Map<String,Dynamic>):Bool
-	{
-		var _a = Lambda.list(args);
-		var _r = new Map();
-		if (matchExpr(_a, expr, opts, _r)) {
-			var rem = [ for (arg in args) if (!Lambda.has(_a, arg)) arg];
-			while (rem.length > 0)
-				args.remove(rem.pop());
-			for (k in _r.keys())
-				res[k] = _r[k];
-			return true;
-		}
-		return false;
-	}
-
 	static function popArgument(args:List<ArgumentToken>)
 	{
 		for (a in args) {
@@ -76,59 +61,64 @@ class Matcher {
 		return null;
 	}
 
-	static function matchExpr(args:List<ArgumentToken>, expr:Expr, opts:Map<String, Option>, res:Map<String,Dynamic>):Bool
+	static function match(args:List<ArgumentToken>, expr:Expr, opts:Map<String, Option>, res:Map<String,Dynamic>):Bool
 	{
 		trace("matching " + expr);
 		if (!expr.match(EOptionals(_) | EEmpty) && args.length < 1)
 			return false;
+		var _args = Lambda.list(args);
+		var _res = new Map<String,Dynamic>();
 		switch (expr) {
 		case EEmpty:  // NOOP
 		case EList(list):
 			for (e in list) {
-				if (!matchExpr(args, e, opts, res))
+				if (!match(_args, e, opts, _res))
 					return false;
 			}
 		case EOptionals(e):
-			tryMatchExpr(args, e, opts, res);
+			match(_args, e, opts, _res);
 		case ERequired(e):
-			return matchExpr(args, e, opts, res);
+			if (!match(args, e, opts, res))
+				return false;
 		case EXor(a, b):
-			return tryMatchExpr(args, a, opts, res) || tryMatchExpr(args, b, opts, res);
+			if (!match(args, a, opts, res) && !match(args, b, opts, res))
+				return false;
 		case EElipsis(EOptionals(e)):
-			tryMatchExpr(args, e, opts, res);
-			while (tryMatchExpr(args, e, opts, res))
+			match(_args, e, opts, _res);
+			while (match(_args, e, opts, _res))
 				true;  // NOOP
 		case EElipsis(e):
 			// TODO deal (somewhere) with the multiple returned values
-			if (!matchExpr(args, e, opts, res))
+			if (!match(_args, e, opts, _res))
 				return false;
-			while (matchExpr(args, e, opts, res))
+			while (match(_args, e, opts, _res))
 				true;  // NOOP
 		case EArgument(arg):
-			var val = popArgument(args);
+			var val = popArgument(_args);
 			if (val == null)
 				return false;
-			res[arg.name] = val;
+			_res[arg.name] = val;
 		case ECommand(name):
-			var val = popArgument(args);
+			var val = popArgument(_args);
 			if (val != name)
 				return false;
-			res[name] = true;
+			_res[name] = true;
 		case EOption(null):
 			// FIXME don't try each option more than once
 			// FIXME don't try options in the pattern
 			// TODO maybe move this in the parser
 			var succeeded = false;
 			for (opt in opts)
-				succeeded = tryMatchExpr(args, EOption(opt), opts, res) || succeeded;
-			return succeeded;
+				succeeded = match(_args, EOption(opt), opts, _res) || succeeded;
+			if (!succeeded)
+				return false;
 		case EOption(opt):
-			var val = popOption(args, opt.names);
+			var val = popOption(_args, opt.names);
 			if (val == null)
 				return false;
 			if (opt.hasParam && val.param == null) {
-				if (args.length > 0) {
-					val.param = popArgument(args);
+				if (_args.length > 0) {
+					val.param = popArgument(_args);
 				} else {
 					return false;
 				}
@@ -140,16 +130,21 @@ class Matcher {
 				}
 			}
 			for (n in opt.names)
-				res[n] = untyped val.param != null ? val.param : true;
+				_res[n] = untyped val.param != null ? val.param : true;
 		}
+		var rem = [ for (a in args) if (!Lambda.has(_args, a)) a];
+		while (rem.length > 0)
+			args.remove(rem.pop());
+		for (k in _res.keys())
+			res[k] = _res[k];
 		return true;
 	}
 
-	public static function match(usage, pat, args)
+	public static function matchPattern(usage, pat, args)
 	{
 		var args = Tokenizer.tokenizeArguments(args, usage.options);
 		var res = makeRes(usage);
-		if (matchExpr(args, pat.pattern, usage.options, res) && args.length == 0)
+		if (match(args, pat.pattern, usage.options, res) && args.length == 0)
 			return res;
 		return null;
 	}
